2f083d8dd96dbac94b08722269c38f270329844b
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-28 16:25:10 -0700

    Edit configs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname, ".")
+  }
 };
 
 export default nextConfig;
