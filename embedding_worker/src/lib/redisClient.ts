33d9894c2aa24dbd666d348b6512abdb8b5d0a27
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-11-20 11:06:08 -0800

    Move redisClient file and use logger

--- embedding_worker/src/lib/redisClient.ts
+++ embedding_worker/src/lib/redisClient.ts
@@ -1,15 +1,16 @@
 import { createClient, RedisClientType } from "redis";
 import config from "@/config/config.ts";
+import logger from "@/config/logger.ts";
 
 export default function redisClientFactory(): RedisClientType {
   const client = createClient({
     url: config.REDIS_URL,
   }) as RedisClientType;
 
-  client.on("error", (err) => console.error("Redis Client Error", err));
+  client.on("error", (err) => logger.error("Redis Client Error", err));
 
   client.on("connect", () => {
-    console.log("Connected to Redis");
+    logger.info("Connected to Redis");
   });
 
   return client;
