7af1846b68c132c49e536eb59644bde1ee2bdecd
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-12-07 16:35:30 -0800

    Make content related fields string instead of json

---
+++ prisma/migrations/20251207214313_make_document_content_string_instead_of_json/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "document" ALTER COLUMN "content" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "document_revision" ALTER COLUMN "content" SET DATA TYPE TEXT;
---
+++ prisma/migrations/20251207214456_remove_document_search_text/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `searchText` on the `document` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "document" DROP COLUMN "searchText";
